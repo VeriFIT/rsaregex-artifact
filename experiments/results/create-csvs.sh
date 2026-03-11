7cb08e2dac38d5c057b22e74a4bc718fdd49e44d
Jan Vašák <xvasak01@vutbr.cz>
2026-03-11 16:58:24 +0100

    fix create-csvs.sh bug and add very short experiment

--- experiments/results/create-csvs.sh
+++ experiments/results/create-csvs.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-cat lf.output | ./pyco_proc --csv > lf.csv
-cat rengar.output | ./pyco_proc --csv > rengar.csv
+cat lf.output | ../pyco_proc --csv > lf.csv
+cat rengar.output | ../pyco_proc --csv > rengar.csv
 
