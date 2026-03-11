04160ca6b738acec79c6ea7565c938ef92110084
Jan Vašák <xvasak01@vutbr.cz>
2026-03-11 14:32:04 +0100

    remove interaction

--- tools-install-files/install-python.sh
+++ tools-install-files/install-python.sh
@@ -13,5 +13,5 @@ rm -rf Python-3.10.12
 
 sudo update-alternatives --install /usr/bin/python python /usr/local/bin/python3.10 2
 sudo update-alternatives --install /usr/bin/python3 python3 /usr/local/bin/python3.10 2 
-sudo update-alternatives --config python
-sudo update-alternatives --config python3
+echo "0" | sudo update-alternatives --config python
+echo "0" | sudo update-alternatives --config python3
